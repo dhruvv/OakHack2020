197e14cc1fd1259f921e2948e80ae670d402bebb
varungiridhar <32874672+varungiridhar@users.noreply.github.com>
2020-01-19 04:12:03 +0530

    4:11AM

--- app/src/main/java/com/example/oakhack2020final/MapsActivity.java
+++ app/src/main/java/com/example/oakhack2020final/MapsActivity.java
@@ -10,6 +10,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -22,22 +24,112 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final String TAG = "MapsActivity";
 
     private GoogleMap mMap;
-    private GoogleApiClient googleApiClient;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
     LocationManager locationManager;
     LocationListener locationListener;
+    long maxid;
+    int flag = 0;
 
-    public void centreMapOnLocation(Location location, String title){
-
+    public void centreMapOnLocation(final Location location, String title){
+        Log.d(TAG, "centreMapOnLocation: ran");
         LatLng userLocation = new LatLng(37.78911, -122.40723);//CHANGE THIS FOR FINAL PRESENTATION
         mMap.clear();
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation ,20));
+        DatabaseReference ref = database.getInstance().getReference();
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                    maxid = dataSnapshot.getChildrenCount();
+                    Log.d(TAG, "onDataChange: "+maxid);
+                    if(flag == 0) {
+                        parseData(maxid);
+                        flag += 1;
+                    }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+
+    }
+
+    private void parseData(long noOfChildren){
+        Log.d(TAG, "parseData: "+noOfChildren);
+        for(int i = 1; i<=noOfChildren; i++) {
+            DatabaseReference mRef = database.getInstance().getReference().child(String.valueOf(i));
+
+            mRef.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    String dataSnapshotStr = dataSnapshot.getValue().toString();
+                    dataSnapshotStr.replace("{", "");
+                    dataSnapshotStr.replace("}", "");
+                    String[] dataSnapshotSplitted = dataSnapshotStr.split("\\s+");
+                    String LAT = dataSnapshotSplitted[0];
+                    String LONG = dataSnapshotSplitted[1];
+
+                    addMarker(LAT, LONG);
+
+
+
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+
+
+            });
+        }
+    }
+
+
+    private void addMarker(String LAT, String LONG){
+
+        int height = 100;
+        int width = 100;
+        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.manager);
+        Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        BitmapDescriptor smallMarkerIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(Double.valueOf(LAT), Double.valueOf(LONG))).icon(smallMarkerIcon));
 
     }
 
@@ -113,6 +205,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     }
+
+
+
+
 
 
 }
